477c296f5cdc87877d27a6a12d3619a6bad8bf5d
何洋 <yang.he@jimubox.com>
2014-09-15 11:45:24 +0800

    add: add myth file and update gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,27 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var stylus = require('gulp-stylus');
 var less = require('gulp-less');
+var myth = require('gulp-myth');
 
 gulp.task('sass', function () {
-    gulp.src('*.scss')
+    gulp.src('./origin/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist'));
 });
 gulp.task('less', function () {
-    gulp.src('*.less')
+    gulp.src('./origin/*.less')
     .pipe(less())
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist'));
 });
 gulp.task('stylus', function () {
-    gulp.src('*.styl')
+    gulp.src('./origin/*.styl')
     .pipe(stylus())
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist'));
+});
+gulp.task('myth', function () {
+    gulp.src('./origin/*.css')
+    .pipe(myth())
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['sass', 'less', 'stylus']);
+gulp.task('default', ['sass', 'less', 'stylus', 'myth']);
