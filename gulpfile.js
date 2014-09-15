f89ab440d4f79df96ab4249f2681de85afb49622
何洋 <yang.he@jimubox.com>
2014-09-15 18:10:34 +0800

    update: add watch listener to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,25 @@ var sass = require('gulp-sass');
 var stylus = require('gulp-stylus');
 var less = require('gulp-less');
 var myth = require('gulp-myth');
+var watch = require('gulp-watch');
 
 gulp.task('sass', function () {
-    gulp.src('./origin/*.scss')
+    watch('./origin/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./dist'));
 });
 gulp.task('less', function () {
-    gulp.src('./origin/*.less')
+    watch('./origin/*.less')
     .pipe(less())
     .pipe(gulp.dest('./dist'));
 });
 gulp.task('stylus', function () {
-    gulp.src('./origin/*.styl')
+    watch('./origin/*.styl')
     .pipe(stylus())
     .pipe(gulp.dest('./dist'));
 });
 gulp.task('myth', function () {
-    gulp.src('./origin/*.css')
+    watch('./origin/*.css')
     .pipe(myth())
     .pipe(gulp.dest('./dist'));
 });
